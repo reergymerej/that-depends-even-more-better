23c4587da08a1e495338955ddf455a109d4ce9ef
Jeremy Greer <jeremy.greer.atl@gmail.com>
2018-04-07 17:22:03 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# that-depends
+
+Look through source, find dependencies.
+
+
+## Usage
+
+```sh
+that-depends path/to/src/
+```
---
+++ index.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+
+// find files (of some type)
+//
+// Get imported files.
+//
+// Do something with the results.
