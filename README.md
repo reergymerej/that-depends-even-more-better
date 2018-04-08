623073dbf88fb650a73c35e37479423e01b5f581
Jeremy Greer <jeremy.greer.atl@gmail.com>
2018-04-07 21:19:47 -0400

    Handle js

--- README.md
+++ README.md
@@ -5,6 +5,16 @@ Look through source, find dependencies.
 
 ## Usage
 
+### CLI
+
 ```sh
 that-depends path/to/src/
+```
+
+### Programatic
+
+```js
+import thatDepends from 'that-depends'
+
+const deps = thatDepends('some/glob/to/src/**/*.js')
 ```
--- src/index.js
+++ src/index.js
@@ -1,14 +1,34 @@
 #!/usr/bin/env node
 
+const fs = require('fs')
+const path = require('path')
 const glob = require('glob')
+const thatDependsJS = require('that-depends-js')
 
-// find files (of some type)
 const pattern = '/Users/jeremygreer/code/work/onq-web/src/**/*.js'
 const files = glob.sync(pattern)
-console.log(files)
 
-// Get imported files.
+const getImports = (extension, file) => {
+  switch (extension) {
+  case '.js':
+    return thatDependsJS(file)
+  default:
+    throw new Error(`"${extension}" is not a supported file type."`)
+  }
+}
 
+const resultMap = {}
+
+files.forEach(filepath => {
+  const file = fs.readFileSync(filepath, 'utf8')
+  const extension = path.extname(filepath)
+  const imports = getImports(extension, file)
+  resultMap[filepath] = imports
+})
 
 // Do something with the results.
-//
+if (require.main === module) {
+  console.log(resultMap) // eslint-disable-line no-console
+} else {
+  return resultMap
+}
