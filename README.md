1e0839cac403af022589b2f088019e80943dee64
Jeremy Greer <jeremy.greer.atl@gmail.com>
2018-04-07 22:22:47 -0400

    Sort, update readme

--- README.md
+++ README.md
@@ -5,16 +5,15 @@ Look through source, find dependencies.
 
 ## Usage
 
-### CLI
-
-```sh
-that-depends path/to/src/
-```
-
-### Programatic
-
 ```js
 import thatDepends from 'that-depends'
 
 const deps = thatDepends('some/glob/to/src/**/*.js')
+```
+
+```
+'/Users/bork/src/modules/oozer.js': [ '../../bingo', './foo' ],
+'/Users/bork/src/modules/user.js': [ './helpers', 'values' ],
+'/Users/bork/src/theme/_utilities.scss': [],
+'/Users/bork/src/user.js': [ './user.js', 'redux', 'values' ],
 ```
--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-#!/usr/bin/env node
-
 const fs = require('fs')
 const path = require('path')
 const glob = require('glob')
 const thatDependsJS = require('that-depends-js')
+const thatDependsSCSS = require('that-depends-scss')
 
-const pattern = '/Users/jeremygreer/code/work/onq-web/src/**/*.js'
+const pattern = '/Users/jeremygreer/code/work/onq-web/src/**/*.{js,scss}'
 const files = glob.sync(pattern)
 
 const getImports = (extension, file) => {
   switch (extension) {
   case '.js':
+  case '.jsx':
     return thatDependsJS(file)
+  case '.scss':
+    return thatDependsSCSS(file)
   default:
     throw new Error(`"${extension}" is not a supported file type."`)
   }
@@ -19,14 +21,13 @@ const getImports = (extension, file) => {
 
 const resultMap = {}
 
-files.forEach(filepath => {
+files.sort().forEach(filepath => {
   const file = fs.readFileSync(filepath, 'utf8')
   const extension = path.extname(filepath)
   const imports = getImports(extension, file)
   resultMap[filepath] = imports
 })
 
-// Do something with the results.
 if (require.main === module) {
   console.log(resultMap) // eslint-disable-line no-console
 } else {
