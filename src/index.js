bc84bf81ad3469b7fc6ed816d5a0744a987b4ec9
Jeremy Greer <jeremy.greer.atl@gmail.com>
2018-04-08 10:40:54 -0400

    Add Jest, determine imports & dependents

--- src/index.js
+++ src/index.js
@@ -19,17 +19,64 @@ const getImports = (extension, file) => {
   }
 }
 
-const resultMap = {}
-
-files.sort().forEach(filepath => {
-  const file = fs.readFileSync(filepath, 'utf8')
-  const extension = path.extname(filepath)
-  const imports = getImports(extension, file)
-  resultMap[filepath] = imports
-})
+const run = () => {
+  const resultMap = {}
+  files.sort().forEach(filepath => {
+    const file = fs.readFileSync(filepath, 'utf8')
+    const extension = path.extname(filepath)
+    const imports = getImports(extension, file)
+    resultMap[filepath] = imports
+  })
+  return resultMap
+}
 
 if (require.main === module) {
-  console.log(resultMap) // eslint-disable-line no-console
-} else {
-  return resultMap
+  const result = run()
+  console.log(result) // eslint-disable-line no-console
+}
+
+const addImports = (withImports) => {
+  const fixed = {
+  }
+
+  Object.keys(withImports).forEach(key => {
+    fixed[key] = {
+      imports: withImports[key],
+    }
+  })
+
+  return fixed
+}
+
+const addDependents = (withImports) => {
+  const fixed = {}
+
+  Object.keys(withImports).forEach(key => {
+    const { imports } = withImports[key]
+
+    // If we recurse, we may have circular dependencies.  :D
+    // We're merging with what's there because we may have built it when adding
+    // the dependents.
+    fixed[key] = fixed[key] || {}
+    fixed[key].imports = fixed[key].imports || []
+    fixed[key].dependents = fixed[key].dependents || []
+
+    // Each import means this is a dependent of the imported file.
+    fixed[key].imports = fixed[key].imports.concat(...imports)
+
+    imports.map(i => {
+      // key is a dependent of i
+      fixed[i] = fixed[i] || {}
+      fixed[i].dependents = fixed[i].dependents || []
+      fixed[i].dependents.push(key)
+    })
+  })
+
+  return fixed
+}
+
+module.exports = {
+  run,
+  addImports,
+  addDependents,
 }
