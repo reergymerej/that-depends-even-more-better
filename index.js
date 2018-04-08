bc9667eda71e19bb19a0bd4254f584bc9214dac7
Jeremy Greer <jeremy.greer.atl@gmail.com>
2018-04-07 20:22:43 -0400

    Add ESLint

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 #!/usr/bin/env node
 
+const glob = require('glob')
 
 // find files (of some type)
+const files = glob.sync(pattern)
+console.log(files)
 //
 // Get imported files.
 //
 // Do something with the results.
+//
